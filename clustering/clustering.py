57fb68867200270780963d7ef658c52041151ffd
bram nuttin <bramnuttin@hotmail.com>
2020-12-16 12:45:41 +0100

    KMedoids & give clusterparam options

--- clustering/clustering.py
+++ clustering/clustering.py
@@ -3,11 +3,14 @@ from clustering.criteria import calc_criteria_ground_truth, silhouette_analysis,
 from tqdm import tqdm
 from sklearn.mixture import GaussianMixture, BayesianGaussianMixture
 from sklearn.cluster import AgglomerativeClustering
+from sklearn_extra.cluster import KMedoids
 
-def run_analysis_kmeans(matrix, trial_mouselines, n_init=200):
+def run_analysis_kmeans(matrix, trial_mouselines, k_range=None, n_init=200, max_iter=500):
     results = {}
-    for k in tqdm(range(2, 15)):
-        kmeans = KMeans(n_clusters=k, init='k-means++', max_iter=100, n_init=n_init)
+    if None:
+        k_range = range(2, 15)
+    for k in tqdm(k_range):
+        kmeans = KMeans(n_clusters=k, init='k-means++', n_init=n_init, max_iter=max_iter)
         kmeans.fit(matrix)
         labels = kmeans.predict(matrix)
         k_results = calc_criteria_ground_truth(labels, trial_mouselines)
@@ -23,11 +26,12 @@ def run_analysis_kmeans(matrix, trial_mouselines, n_init=200):
 
     return results
 
-def run_analysis_gaussian_mixture(matrix, trial_mouselines, n_init=200):
+def run_analysis_gaussian_mixture(matrix, trial_mouselines, n_init=200, k_range=None, max_iter=500):
     results = {}
-
-    for k in tqdm(range(2, 15)):
-        mixt = GaussianMixture(n_components=k, n_init=n_init)
+    if None:
+        k_range = range(2, 15)
+    for k in tqdm(k_range):
+        mixt = GaussianMixture(n_components=k, n_init=n_init, max_iter=max_iter)
         mixt.fit(matrix)
 
         labels = mixt.predict(matrix)
@@ -66,13 +70,50 @@ def run_analysis_agglomerative(matrix, trial_mouselines):
 
     return results
 
-def run_kmeans(matrix, n_clusters=5, n_init=1000):
-    kmeans = KMeans(n_clusters=n_clusters, init='k-means++', max_iter=1000, n_init=n_init)
+def run_analysis_kmedoids(matrix, trial_mouselines, k_range=None, init='heuristic', n_init=100, metric='euclidean', max_iter=300):
+    results = {}
+    if None:
+        k_range = range(2, 15)
+    for k in tqdm(k_range):
+        k_results = None
+        min_inertia = float('inf')
+        for i in range(n_init):
+            kmedoids = KMedoids(n_clusters=k, init=init, metric=metric, max_iter=max_iter)
+            kmedoids.fit(matrix)
+            if kmedoids.inertia_ < min_inertia:
+                min_inertia = kmedoids.inertia_
+                labels = kmedoids.predict(matrix)
+                k_results = calc_criteria_ground_truth(labels, trial_mouselines)
+                k_results['ssq'] = kmedoids.inertia_
+                k_results['silhouette'] = silhouette_analysis(matrix, labels)
+                k_results['davies_bouldin'] = davies_bouldin(matrix, labels)
+        if results == {}:
+            for crit, res in k_results.items():
+                results[crit] = [res]
+        else:
+            for crit, res in k_results.items():
+                results[crit].append(res)
+
+    return results
+
+def run_kmeans(matrix, n_clusters=5, n_init=1000, max_iter=1000):
+    kmeans = KMeans(n_clusters=n_clusters, init='k-means++', max_iter=max_iter, n_init=n_init)
     kmeans.fit(matrix)
     return kmeans
 
-def run_gaussian_mixture(matrix, n_components=5, n_init=1000):
-    mixt = GaussianMixture(n_components=n_components, n_init=n_init)
+def run_kmedoids(matrix, n_clusters=5, n_init=1000, metric='euclidean', max_iter=1000, init='heuristic'):
+    best_kmedoids = None
+    min_inertia = float('inf')
+    for i in range(n_init):
+        kmedoids = KMedoids(n_clusters=n_clusters, init=init, max_iter=max_iter, metric=metric)
+        kmedoids.fit(matrix)
+        if kmedoids.inertia_ < min_inertia:
+            min_inertia = kmedoids.inertia_
+            best_kmedoids = kmedoids
+    return best_kmedoids
+
+def run_gaussian_mixture(matrix, n_components=5, n_init=1000, max_iter=100):
+    mixt = GaussianMixture(n_components=n_components, n_init=n_init, max_iter=max_iter)
     mixt.fit(matrix)
     return mixt
 
